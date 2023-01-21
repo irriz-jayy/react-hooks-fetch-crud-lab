30d5e8994cda1495748e8b6654d2a35ed3b8fee2
irriz-jayy <jayfelix789@gmail.com>
2023-01-21 18:37:31 +0300

    assignement complete

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questionData, setQuestionData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then((data) => setQuestionData(data));
+  }, []);
+
+  const questionList = questionData.map((questionObj) => {
+    return (
+      <QuestionItem
+        onUpdateQuestion={handleUpdate}
+        key={questionObj.id}
+        question={questionObj}
+        onDeleteQuestion={handleDeleteQuestion}
+      />
+    );
+  });
+  function handleUpdate(updatedAnswer) {
+    const updatedQuestions = questionData.map((question) => {
+      if (question.correctIndex === updatedAnswer.correctIndex) {
+        return question;
+      } else {
+        return updatedAnswer;
+      }
+    });
+    setQuestionData(updatedQuestions);
+  }
+
+  function handleDeleteQuestion(question) {
+    const afterDeleteData = questionData.filter(
+      (obj) => obj.id !== question.id
+    );
+    setQuestionData(afterDeleteData);
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionList}</ul>
     </section>
   );
 }
